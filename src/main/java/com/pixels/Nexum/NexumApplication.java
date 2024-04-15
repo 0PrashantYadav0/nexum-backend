a3fd903b2ac32f6b4393dbe8641b0aaf2cc6545d
Prashant <prashantyadav09783@gmail.com>
2024-04-15 18:17:02 +0530

    spring boot initail project

---
+++ src/main/java/com/pixels/Nexum/NexumApplication.java
@@ -0,0 +1,13 @@
+package com.pixels.Nexum;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NexumApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NexumApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pixels/Nexum/NexumApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pixels.Nexum;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NexumApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

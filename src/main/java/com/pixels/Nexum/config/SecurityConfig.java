7683aa9eadd16e93f8695614970de086257f03da
Prashant <prashantyadav09783@gmail.com>
2024-04-22 09:06:48 +0530

    worker route protected

--- src/main/java/com/pixels/Nexum/config/SecurityConfig.java
+++ src/main/java/com/pixels/Nexum/config/SecurityConfig.java
@@ -43,7 +43,7 @@ public class SecurityConfig {
         return http
                 .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(
-                        req->req.requestMatchers("/api/login/**", "/api/worker/**",  "/api/google/**", "/api/register/**", "/api/review/**", "/api/message/**")
+                        req->req.requestMatchers("/api/login/**",  "/api/google/**", "/api/register/**", "/api/review/**", "/api/message/**")
                                 .permitAll()
                                 .requestMatchers("/api/worker/**").hasAuthority("WORK")
                                 .anyRequest()
